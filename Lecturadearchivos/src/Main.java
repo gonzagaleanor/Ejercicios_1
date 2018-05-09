714e24c7bec690aaf7eb1a88ba349c77d6d91084
7mo tecnica <7mo tecnica@192.168.0.154>
2018-05-09 11:41:55 -0300

    Modificacion tp1

--- Lecturadearchivos/src/Main.java
+++ Lecturadearchivos/src/Main.java
@@ -6,7 +6,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import ipe.utils.OrderMode;
+import ipe.utils.OrderedMap;
 
 public class Main {
 	char caracter;
@@ -24,8 +30,24 @@ public class Main {
 
 	private static void mostrarResultado() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("::::::TODOS::::::");
+		for (String string : contadorPalabras.keySet()) {
+			System.out.println(string.toLowerCase());
+		}
 
+		System.out.println("::::::ORDENADOS::::::");
+		LinkedHashMap<String, Integer> ContadorOrdenado = OrderedMap.sortMapByValue(contadorPalabras,
+				OrderMode.DESCENDING);
+		int cont = 1;
+		Set<Entry<String, Integer>> entrySetSortedByValue = ContadorOrdenado.entrySet();
+		for (Entry<String, Integer> mapping : entrySetSortedByValue) {
+			if (cont < 11) {
+				System.out.println(cont + "\t" + mapping.getKey() + "\t" + mapping.getValue());
+				cont++;
+			} else {
+				break;
+			}
+		}
 	}
 
 	private static void contarPalabras() {
@@ -36,20 +58,20 @@ public class Main {
 			String[] palabras = identificarPalabra(cont);
 			contarPalabras(palabras);
 		}
-		
 
 	}
 
 	private static void contarPalabras(String[] palabras) {
-			for (String p : palabras) {
-				if(contadorPalabras.get(p)==null) {
-					contadorPalabras.put(p, 1);
-				}
-				else {
-					int contador=contadorPalabras.get(p)+1;
-					contadorPalabras.put(p, contador);
-				}
+		for (String p : palabras) {
+			String comparador = p.toLowerCase();
+			// String comparador = p;
+			if (contadorPalabras.get(comparador) == null) {
+				contadorPalabras.put(comparador, 1);
+			} else {
+				int contador = contadorPalabras.get(comparador) + 1;
+				contadorPalabras.put(comparador, contador);
 			}
+		}
 
 	}
 
@@ -58,9 +80,22 @@ public class Main {
 		int inicio = 0;
 		for (int i = 0; i < contenido.length(); i++) {
 			String palabra = "";
-			if (contenido.charAt(i) == ' ') {
+			boolean separador = esSeparador(contenido.charAt(i));
+			int separadorExtra = 0;
+			if (separador || i + 1 == contenido.length()) {
+				while (i+1 < contenido.length() && esSeparador(contenido.charAt(i+1))) {
+					i++;
+					separadorExtra++;
+				}
+				
 				if (i > 0) {
-					palabra = contenido.substring(inicio, i);
+					int fin;
+					if (separador) {
+						fin = i - separadorExtra;
+					} else {
+						fin = i + 1;
+					}
+					palabra = contenido.substring(inicio, fin);
 					inicio = i + 1;
 					palabras.add(palabra);
 				}
@@ -70,9 +105,24 @@ public class Main {
 		return palabras.toArray(new String[0]);
 	}
 
+	private static boolean esSeparador(char charAt) {
+		if (charAt == ' ')
+			return true;
+		else if (charAt == '.')
+			return true;
+		else if (charAt == ',')
+			return true;
+		else if (charAt == '!')
+			return true;
+		else if (charAt == '?')
+			return true;
+		
+		return false;
+	}
+
 	public static void leerArchivo() {
 		// Creo un archivo de texto con el objeto File
-		File file = new File("C:\\Users\\7mo tecnica\\Documents\\Franzofeo.txt");
+		File file = new File("C:\\Users\\7mo tecnica\\Documents\\textocopia2.txt");
 
 		contenidoArchivo = new ArrayList<String>();
 		// Defino la referencia de un Scanner
@@ -110,24 +160,4 @@ public class Main {
 		}
 		return contador;
 	}
-
-	// // Otra alternativa, es utilizar un BufferedReader
-	// BufferedReader br = null;
-	//
-	// try {
-	// // Creo el objeto en base al file, pero previamente lo tengo que
-	// // leer a través de un FileReader
-	// br = new BufferedReader(new FileReader(file));
-	//
-	// // readLine() lee la siguiente línea, en caso que no haya nada para
-	// // leer devuelve null
-	// String st;
-	// while ((st = br.readLine()) != null)
-	// System.out.println(st);
-	//
-	// // cierro el archivo
-	// br.close();
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
 }
